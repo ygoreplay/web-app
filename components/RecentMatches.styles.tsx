e4378ca58db911b95c2488f85e859819c9c0cdb3
Sophia <beyondthesophia@gmail.com>
2021-10-13 18:28:44 +0900

    feat(ui): 최근 매치 UI 구현

--- components/RecentMatches.styles.tsx
+++ components/RecentMatches.styles.tsx
@@ -4,3 +4,40 @@ export const Root = styled.div`
     margin: 0;
     padding: 0;
 `;
+
+export const Item = styled.div`
+    padding: ${({ theme }) => theme.spacing(1.5)};
+
+    display: flex;
+`;
+
+export const Entry = styled.div`
+    display: flex;
+    flex: 1 1 0;
+
+    &:first-of-type {
+        justify-content: flex-end;
+    }
+
+    &:last-of-type {
+        flex-direction: row-reverse;
+        justify-content: flex-end;
+    }
+`;
+
+export const Symbol = styled.div`
+    margin: 0 ${({ theme }) => theme.spacing(1.5)};
+    flex: 0 0 auto;
+`;
+
+export const PlayerName = styled.span`
+    margin: 0 ${({ theme }) => theme.spacing(1)};
+
+    display: block;
+
+    opacity: 0.5;
+`;
+
+export const DeckName = styled.span`
+    display: block;
+`;
--- components/RecentMatches.tsx
+++ components/RecentMatches.tsx
@@ -1,16 +1,88 @@
+import * as _ from "lodash";
 import React from "react";
 
-import { Root } from "./RecentMatches.styles";
+import { OnSubscriptionDataOptions } from "@apollo/client";
 
-interface RecentMatchesProps {}
-interface RecentMatchesStates {}
+import { MatchesProps, NewMatchCreatedComponent, NewMatchCreatedSubscription, withMatches } from "@query";
 
-export default class RecentMatches extends React.Component<RecentMatchesProps, RecentMatchesStates> {
-    public render() {
+import { DeckName, Entry, Item, PlayerName, Root, Symbol } from "@components/RecentMatches.styles";
+
+interface RecentMatchesStates {
+    matches: Array<Exclude<MatchesProps["data"]["matches"], undefined>[0]> | null;
+}
+
+class RecentMatches extends React.Component<MatchesProps, RecentMatchesStates> {
+    public state: RecentMatchesStates = {
+        matches: null,
+    };
+
+    public componentDidUpdate(prevProps: Readonly<MatchesProps>) {
+        if (this.props.data.loading !== prevProps.data.loading && !this.props.data.loading && this.props.data.matches) {
+            // eslint-disable-next-line react/no-did-update-set-state
+            this.setState({
+                matches: [...this.props.data.matches],
+            });
+        }
+    }
+
+    private handleNewMatchCreated = ({ subscriptionData }: OnSubscriptionDataOptions<NewMatchCreatedSubscription>) => {
+        if (!subscriptionData.data) {
+            return;
+        }
+
+        if (!this.state.matches) {
+            return;
+        }
+
+        console.info(subscriptionData.data.newMatchCreated.id);
+
+        this.setState((prevState: RecentMatchesStates) => ({
+            matches: prevState.matches ? _.uniqBy([subscriptionData.data!.newMatchCreated, ...prevState.matches], item => item.id).slice(0, 10) : null,
+        }));
+    };
+
+    private renderLoading = () => {
+        return "loading";
+    };
+    private renderMatch = (match: Exclude<MatchesProps["data"]["matches"], undefined>[0]) => {
+        const firstRound = match.rounds[0];
+        const playerNames = firstRound.playerDecks;
+
+        return (
+            <Item key={match.id}>
+                <Entry>
+                    <PlayerName>{playerNames[0].player.name}</PlayerName>
+                    <DeckName>{playerNames[0].deck.recognizedName}</DeckName>
+                </Entry>
+                <Symbol>vs</Symbol>
+                <Entry>
+                    <PlayerName>{playerNames[1].player.name}</PlayerName>
+                    <DeckName>{playerNames[1].deck.recognizedName}</DeckName>
+                </Entry>
+            </Item>
+        );
+    };
+    private renderItems = (matches: Exclude<MatchesProps["data"]["matches"], undefined>) => {
+        return matches.map(this.renderMatch);
+    };
+
+    render() {
+        const { matches } = this.state;
+
         return (
             <Root>
-                <span>RecentMatches</span>
+                {matches && <NewMatchCreatedComponent onSubscriptionData={this.handleNewMatchCreated} />}
+                {!matches && this.renderLoading()}
+                {matches && this.renderItems(matches)}
             </Root>
         );
     }
 }
+
+export default withMatches({
+    options: {
+        variables: {
+            count: 10,
+        },
+    },
+})(RecentMatches);
