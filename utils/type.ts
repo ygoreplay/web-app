efe4b226bd3793762557d96800f28af5aa62347a
Sophia <beyondthesophia@gmail.com>
2021-10-16 20:33:33 +0900

    refactor(type): 타입 정의 수정

--- utils/type.ts
+++ utils/type.ts
@@ -3,4 +3,4 @@ import { MatchDetailedQuery, MatchesQuery } from "queries/index";
 export type Match = MatchesQuery["matches"][0];
 export type MatchDetail = Exclude<MatchDetailedQuery["match"], null | undefined>;
 
-export type DeckBase = Pick<MatchDetail["rounds"][0]["playerDecks"][0]["deck"], "side" | "main" | "extra" | "recognizedName">;
+export type DeckBase = Pick<MatchDetail["home"]["deck"], "side" | "main" | "extra" | "recognizedName">;
