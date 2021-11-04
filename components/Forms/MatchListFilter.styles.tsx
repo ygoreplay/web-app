245b1ff5b258246024d4a29909b6ce4163ab9d8b
Sophia <beyondthesophia@gmail.com>
2021-11-04 14:24:53 +0900

    refactor(match): 매치 리스트 필터링 UI 수정

--- components/Forms/MatchListFilter.styles.tsx
+++ components/Forms/MatchListFilter.styles.tsx
@@ -6,13 +6,13 @@ export const Root = styled.form`
     margin-right: ${({ theme }) => theme.spacing(2)};
 
     position: sticky;
-    top: ${({ theme }) => theme.spacing(8)};
+    top: ${({ theme }) => theme.spacing(9)};
 
     @media (min-width: 0px) and (orientation: landscape) {
-        top: ${({ theme }) => theme.spacing(7)};
+        top: ${({ theme }) => theme.spacing(8)};
     }
 
     @media (min-width: 600px) {
-        top: ${({ theme }) => theme.spacing(9)};
+        top: ${({ theme }) => theme.spacing(10)};
     }
 `;
