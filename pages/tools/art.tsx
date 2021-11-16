7fdf0b9133eb6459784dca074e93e796db47960b
Sophia <beyondthesophia@gmail.com>
2021-11-16 13:47:20 +0900

    fix(cropper): 타입 에러 수정

--- pages/tools/art.tsx
+++ pages/tools/art.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from "next";
 
-import { useApolloClient } from "@apollo/client";
 import { ThemeProvider } from "@emotion/react";
 
 import AdminArtRoute from "@routes/tools/art";
@@ -8,11 +7,9 @@ import AdminArtRoute from "@routes/tools/art";
 import { toolTheme } from "@styles/theme";
 
 const AdminArt: NextPage = () => {
-    const client = useApolloClient();
-
     return (
         <ThemeProvider theme={toolTheme}>
-            <AdminArtRoute client={client} />
+            <AdminArtRoute />
         </ThemeProvider>
     );
 };
